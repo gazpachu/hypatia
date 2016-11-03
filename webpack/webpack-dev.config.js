919ea9881f0faaddf197d1a8bf1b88cae919386b
Joan Mira <joan.mira@vodafone.com>
2016-11-03 10:30:55 +0100

    Added github website

--- webpack/webpack-dev.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = require('./webpack.config.js')({
-  isProduction: false,
-  devtool: 'cheap-eval-source-map',
-  jsFileName: 'app.js',
-  cssFileName: 'app.css',
-  port: 3000,
-});
--- webpack/webpack-prod.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('./webpack.config.js')({
-  isProduction: true,
-  devtool: 'source-map',
-  jsFileName: 'app.[hash].js',
-  cssFileName: 'app.[hash].css',
-});
