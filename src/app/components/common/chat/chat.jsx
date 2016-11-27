0000fc6c563f97c37394e111d99e231e442c8c78
Joan Mira <joan.mira@vodafone.com>
2016-11-27 15:15:32 +0100

    working on slack integration

--- src/app/components/common/chat/chat.jsx
+++ src/app/components/common/chat/chat.jsx
@@ -4,18 +4,18 @@ import classNames from 'classnames';
 import {connect} from 'react-redux';
 import { rtm, channels, chat } from 'slack';
 import { load as emojiLoader, parse as emojiParser } from 'gh-emoji';
+import { slackConfig } from '../../../constants/slack';
 import $ from 'jquery';
 import moment from 'moment';
 import User from './user';
 import Icon from '../lib/icon/icon'; 
 
 const defaultProps = {
-	apiToken: 'xoxb-109360821427-CsCZVGK9c6BBFlIlIw3plGXq'
+	
 };
 
 const propTypes = {
-	isDesktop: PropTypes.bool,
-	apiToken: PropTypes.string.isRequired
+	isDesktop: PropTypes.bool
 };
 
 class Chat extends Component {
@@ -28,8 +28,9 @@ class Chat extends Component {
 		
 		this.state = {
 			failed: false,
-			onlineUsers: [],
+			users: [],
 			channels: [],
+			defaultChannelId: null,
 			messages: [],
 			postMyMessage: '',
 			postMyFile: '',
@@ -41,7 +42,7 @@ class Chat extends Component {
 		};
 		
 		// Set class variables
-		this.refreshTime = 2000;
+		this.refreshTime = 5000;
 		this.activeChannel = [];
 		this.activeChannelInterval = null;
 		this.messageFormatter = {
@@ -55,16 +56,27 @@ class Chat extends Component {
 		// Connect bot
 		this.connectBot(this).then((data) => {
 			this.debugLog('got data', data);
-			this.setState({ onlineUsers: data.onlineUsers, channels: data.channels });
+			this.setState({ users: data.users, channels: data.channels, defaultChannelId: data.defaultChannelId });
+			this.loadMessages();
 		})
 		.catch((err) => {
 			this.debugLog('could not intialize slack bot', err);
 			this.setState({ failed: true });
 		});
+		
+		this.loadMessages = this.loadMessages.bind(this);
+		this.formatMessage = this.formatMessage.bind(this);
 	}
 	
 	componentDidMount() {
 		
+	}
+	
+	componentWillUnmount() {
+		if (this.activeChannelInterval) {
+			clearInterval(this.activeChannelInterval);
+			this.activeChannelInterval = null;
+		}
 	}
 	
 	arraysIdentical(a, b) {
@@ -89,27 +101,89 @@ class Chat extends Component {
 				this.bot.started((payload) => {
 					this.debugLog(payload);
 
-					// Create new User object for each online user found
-					// Add to our list only if the user is valid
-					const onlineUsers = [];
-
-					// extract and resolve return the users
-					payload.users.map((user) => !user.is_bot ? onlineUsers.push(new User(user)) : null);
+					const users = [];
+					payload.users.map((user) => !user.is_bot ? users.push(new User(user)) : null);
 
 					const channels = [];
+					let defaultChannelId = null;
 					payload.channels.map((channel) => {
+						if (channel.name === 'general') defaultChannelId = channel.id;
 						channels.push(channel);
 					});
-					return resolve({ channels, onlineUsers });
+					
+					return resolve({ channels, users, defaultChannelId });
 				});
 
 				// tell the bot to listen
-				this.bot.listen({ token: this.props.apiToken });
+				this.bot.listen({ token: slackConfig.apiToken });
+				
+				this.bot.user_typing(function(msg) {
+				  	console.log('several people are coding', msg)
+				});
 			}
 			catch (err) {
 				return reject(err);
 		  	}
 		});
+	}
+	
+	loadMessages(channelId) {
+		const that = this;
+		
+		// define loadMessages function
+		const getMessagesFromSlack = () => {
+			const messagesLength = that.state.messages.length;
+			channels.history({
+				token: slackConfig.apiToken,
+				channel: channelId || this.state.defaultChannelId
+			}, (err, data) => {
+				if (err) {
+					this.debugLog(`There was an error loading messages for ${channelId}. ${err}`);
+					return this.setState({ failed: true });
+				}
+				
+				// loaded channel history
+				this.debugLog('got data', data);
+				
+				// Scroll down only if the stored messages and received messages are not the same
+				// reverse() mutates the array
+				if (!this.arraysIdentical(this.state.messages, data.messages.reverse())) {
+					// Got new messages
+					return this.setState({ messages: data.messages}, () => {
+						// if div is already scrolled to bottom, scroll down again just incase a new message has arrived
+						$('.messages').scrollTop($('.messages').height());
+					});
+				}
+				
+				return;
+			});
+		};
+		
+		getMessagesFromSlack();
+		
+		// Set the function to be called at regular intervals
+		// get the history of channel at regular intevals
+		this.activeChannelInterval = setInterval(getMessagesFromSlack, this.refreshTime);
+	}
+	
+	formatMessage(message, i) {
+		let thisUser = {name: '', image: ''},
+			sameUser = (i>1 && this.state.messages[i-1].user === message.user) ? true : false;
+
+		this.state.users.map((user) => {
+			if (user.id === message.user) thisUser = user;
+		});
+		
+		return <li key={i} className="message clearfix">
+			<div className="user-image">
+				{(!sameUser) ? <img src={thisUser.image} alt={thisUser.name} width="35" height="35" /> : ''}
+			</div>
+			<div className="content">
+				{(!sameUser) ? <span className="user-name">{thisUser.name}</span> : ''}
+				{(!sameUser) ? <span className="timestamp">{moment(message.ts).format('D/M/YYYY')}</span> : ''}
+				<div className="text">{message.text}</div>
+			</div>
+		</li>;
 	}
 	
 	debugLog(...args) {
@@ -134,14 +208,16 @@ class Chat extends Component {
 					</ul>
 					<h3 className="sidebar-heading">Direct messages</h3>
 					<ul className="users">
-						{this.state.onlineUsers.map((user, i) => <li key={i} className="user">• {user.name}</li>)}
+						{this.state.users.map((user, i) => <li key={i} id={user.id} className={`user ${user.presence}`}>• {user.name}</li>)}
 					</ul>
 				</div>
 				
 				<div className="messages-wrapper">
 					<h2 className="channel-title">MATHS #general</h2>
-					<div className="messages"></div>
-					<input type="text" className="message" />
+					<ul className="messages">
+						{this.state.messages.map((message, i) => this.formatMessage(message, i))}
+					</ul>
+					<input type="text" className="new-message" />
 				</div>
             </section>
 		)
